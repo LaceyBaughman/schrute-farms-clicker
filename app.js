c90de373cdb3960f27bc48f2deb63c35892e87fe
LaceyBaughman <laceyebaughman@gmail.com>
2022-02-12 15:25:53 -0700

    it works, kinda

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const shrute = {
-  produceCount: 0,
-  bucks: 0,
-}
+
 
 let clickUpgrades = {
   manure: {
-    price: 100,
+    cost: 10,
     quantity: 0,
-    multiplier: 1
+    multiplier: 1,
+    purchased: false
   }
 };
 
-let automaticUpgrades = {
-  cousinMose: {
-    price: 60,
+let autoUpgrades = {
+  mose: {
+    cost: 15,
     quantity: 0,
-    multiplier: 20
+    multiplier: 20,
+    purchased: false
   }
 };
+
+let shruteBucks = clickUpgrades += autoUpgrades
 
 function drawBeets() {
   let template = ''
   template += `
-  <img onclick="harvest()" src="/assets/images/beet-plot.png">
+  <img class="beet-plot selectable" onclick="harvest()" src="/assets/images/beet-plot.png">
   `
   document.getElementById('beetPlot').innerHTML = template
 }
@@ -30,87 +31,76 @@ function drawBeets() {
 function drawStats() {
   let template = ''
   template += `
-  <div class="m-2 p-2 text-center">
-  <h1>Beets Harvested: ${shrute.produceCount}</h1>
-  <p>
-  <h1>Shrute Bucks: ${shrute.bucks}</h1>
-  </p>
-  <button onclick="purchaseUpgrade()"><h3>Apply Manure</h3></button><p>
-  <button onclick="purchaseCompanion()"><h3>Buy Cousin Mose for $60 Shrute Bucks</h3></button><p>
-</div>
-  `
+  
+  <p><h1>Shrute Bucks: ${shruteBucks}</h1></p>
+  <p><h3>Bags of Manure Applied: ${clickUpgrades.manure.quantity}</h1></p>
+  <p><button class="btn" onclick="buyManure()"><h3>Apply Manure for<p>$${clickUpgrades.manure.cost} Shrute Bucks</p></h3></button></p>
+  <p><button class="btn" onclick="hireShrute()"><h3>Hire Cousin Mose for<p>$${autoUpgrades.mose.cost} Shrute Bucks</p></h3></button></p>
 
+  `
   document.getElementById('stats').innerHTML = template
 }
 
-
-// function harvest() {
-//   shrute.produceCount++
-//   if (shrute.produceCount >= 10) {
-//     shrute.produce -= 10
-//     shrute.bucks += 50
-//   }
-
-
 function harvest() {
-  shrute.bucks++
-  collectClickUpgrades()
-  // let mose = automaticUpgrades.cousinMose
-  // if (mose.purchased) {
+  let manure = clickUpgrades.manure.quantity * clickUpgrades.manure.multiplier
+  let mose = autoUpgrades.mose.multiplier * autoUpgrades.mose.quantity
+  shruteBucks += manure + mose
+  shruteBucks++
 
-  // }
-  // let manure = clickUpgrades.manure
-  // if (manure.purchased) {
-  //   shrute.bucks += 4
-  // }
   drawStats()
 }
 
+function buyManure() {
+  let manure = clickUpgrades.manure
 
+  if (!manure) { return console.log('Does Not Exist') }
+  if (shruteBucks < manure.cost) { return console.log('Not enough Shrute Bucks') }
 
+  manure.purchased = true
+  shruteBucks -= manure.cost
+  manure.quantity++
+  manure.multiplier++
+  manure.cost += manure.quantity * manure.multiplier
 
-function purchaseAutoUpgrade(Mose) {
-  let mose = automaticUpgrades.cousinMose
+  drawStats()
+}
+// increase quantity of manure
+// increase cost of manure
 
+function hireShrute() {
+  let mose = autoUpgrades.mose
   if (!mose) { return console.log('Does Not Exist') }
-  if (shrute.bucks < mose.price) { return console.log('Not enough Shrute Bucks') }
-  if (mose.purchased) { return console.log('Already did that') }
+  if (shruteBucks < mose.cost) { return console.log('Not enough Shrute Bucks') }
 
   mose.purchased = true
-  shrute.bucks -= mose.cost
-
+  shruteBucks -= mose.cost
+  mose.quantity++
+  mose.multiplier * 2
+  mose.cost += mose.quantity * mose.multiplier
   drawStats()
 }
 
-function purchaseManureUpgrade() {
-  let manure = clickUpgrades.manure
-  if (!manure) { return console.log('Does Not Exist') }
-  if (shrute.bucks < manure.price) { return console.log('Not enough Shrute Bucks') }
-  // if (manure.purchased) { return console.log('Already did that') }
-
-  // manure.purchased = true
-
-  shrute.bucks -= manure.cost
-  // increase quantity of manure
-  // increase cost of manure
-
-  drawStats()
+function autoHarvest() {
+  let moseQ = clickUpgrades.mose.quantity
+  let moseM = clickUpgrades.manure.multiplier
+  if (autoUpgrades.mose.purchased = true) {
+    shruteBucks += moseQ * moseM
+    shruteBucks++
+  }
+  drawStats
 }
 
+// TODO itterate over clickUpgrades (for in)
+//        shruteBucks += upgrade.quantity * upgrade.multiplier
 
+//   setInterval(collectAutoUpgrades, 2000)
 
 function drawAll() {
   drawBeets()
   drawStats()
 }
 
+setInterval(harvest, 3000)
+
 
 drawAll()
-
-function collectClickUpgrades() {
-  // TODO itterate over clickUpgrades (for in)
-  //        shrute.bucks += upgrade.quantity * upgrade.multiplier
-}
-  //   setInterval(collectAutoUpgrades, 2000)
-
-
